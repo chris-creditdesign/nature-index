76a8a1003102839af8f8e69a97ba6b4af3640a75
Chris Ryan <creditdesign@gmail.com>
2013-04-28 09:32:08 +0100

    Add and remove bars via checkboxes but enter bars don't appear in the correct possion

--- js/index.js
+++ js/index.js
@@ -9,9 +9,11 @@ var count = "cc"
 /*	Global variable to hold the cc or ac choice */
 var field = "all"
 /*	Global variable to control which year to disylay */
-var displayYear = 2012;
+var displayYear = "2012";
 /*	Global Array to hold all the data we currently want to display */
 var displayArray = [];
+var yearArray = [];
+var checkArray = [];
 /*	Global variables to control field and count text in axis label */
 var displayCount = "Corrected count";
 var displayField = "All fields";
@@ -104,28 +106,28 @@ function draw(data) {
 			return "<input type='checkbox' value='" + d.country + "' checked>" + d.country;
 		});
 
-	d3.selectAll(".country-select input").on("change", update);		
+	d3.selectAll(".country-select input").on("change", updateDisplayArray);		
 
-	function update() {
-		console.log(this.value);
+	// function update() {
+	// 	console.log(this.value);
 
-		console.log(displayArray.length);
+	// 	console.log(displayArray.length);
 
-		displayArray.shift();
+	// 	displayArray.shift();
 
-		console.log(displayArray.length);
+	// 	console.log(displayArray.length);
 
-		/* Redefine the X Ordinal Scale */
-		xScale.domain(displayArray.map(function(d) { return d.country; }))
-			.rangeRoundBands([margin.left,(width + margin.right)], 0.1);	
+	// 	/* Redefine the X Ordinal Scale */
+	// 	xScale.domain(displayArray.map(function(d) { return d.country; }))
+	// 		.rangeRoundBands([margin.left,(width + margin.right)], 0.1);	
 
-		//Draw X axis
-		svg.transition()
-			.duration(duration)
-			.call(xAxis);
+	// 	//Draw X axis
+	// 	svg.transition()
+	// 		.duration(duration)
+	// 		.call(xAxis);
 
-		updateBars();
-	}
+	// 	updateBars();
+	// }
 
 
 	/*	When a field button is clicked update the field variable to represent the selected 
@@ -157,111 +159,133 @@ function draw(data) {
 	function updateDisplayArray() {
 
 		displayArray = [];
+		yearArray = [];
+		checkArray = [];
 
 		switch (displayYear) { 
 			case "2008":
-				displayArray = data.year2008;
+				yearArray = data.year2008.slice(0);
 				break;
 			case "2009":
-				displayArray = data.year2009;
+				yearArray = data.year2009.slice(0);
 				break;
 			case "2010":
-				displayArray = data.year2010;
+				yearArray = data.year2010.slice(0);
 				break;
 			case "2011":
-				displayArray = data.year2011;
+				yearArray = data.year2011.slice(0);
 				break;
 			case "2012":
-				displayArray = data.year2012;
+				yearArray = data.year2012.slice(0);
 				break; 											 
 			default:
-				displayArray = data.year2012;	
+				yearArray = data.year2012.slice(0);
 				break;		
+		}
+
+
+		for (var i = 0; i < yearArray.length; i++) { 
+				checkArray.push({});
 		}
 
 		if (count === "cc") {
 			yScale.domain([0, d3.max(data.year2012, function(d) { return d.cc;} )]);
 			switch (field) {
 				case "all":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].cc;
+					for (var i = 0; i < checkArray.length; i++) {
+
+						checkArray[i].choice = yearArray[i].cc;
+						checkArray[i].country = yearArray[i].country;
 					};
 					break;
 				case "phys":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].ccPhys;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].ccPhys;
+						checkArray[i].country = yearArray[i].country;
 					};				
 					break;
 				case "life":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].ccLife;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].ccLife;
+						checkArray[i].country = yearArray[i].country;
 					};
 					break;
 				case "earth":
 					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].ccEarth;
+						displayArray[i].choice = yearArray[i].ccEarth;
+						displayArray[i].country = yearArray[i].country;
 					};
 					break;
 				case "chem":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].ccChem;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].ccChem;
+						checkArray[i].country = yearArray[i].country;
 					};
 				break;
 				default:
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].cc;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].cc;
+						checkArray[i].country = yearArray[i].country;
 					};										
 			}
 		} else if (count === "ac"){
 			yScale.domain([0, d3.max(data.year2012, function(d) { return d.ac;} )]);
 			switch (field) {
 				case "all":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].ac;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].ac;
+						checkArray[i].country = yearArray[i].country;
 					};
 					break;
 				case "phys":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].acPhys;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].acPhys;
+						checkArray[i].country = yearArray[i].country;
 					};				
 					break;
 				case "life":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].acLife;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].acLife;
+						checkArray[i].country = yearArray[i].country;
 					};
 					break;
 				case "earth":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].acEarth;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].acEarth;
+						checkArray[i].country = yearArray[i].country;
 					};
 					break;
 				case "chem":
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].acChem;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].acChem;
+						checkArray[i].country = yearArray[i].country;
 					};
 				break;
 				default:
-					for (var i = 0; i < displayArray.length; i++) {
-						displayArray[i].choice = displayArray[i].ac;
+					for (var i = 0; i < checkArray.length; i++) {
+						checkArray[i].choice = yearArray[i].ac;
+						checkArray[i].country = yearArray[i].country;
 					};										
 			}
 		};
 
-		// for (var i = 0; i < displayArray.length; i++) {
-		// 	var countryName = displayArray[i].country;
-		// };
 
-		/* Try to loop through all the objects and remove them from the array if their corresponding
-			chectbox is unchecked */
 
-		if (d3.select(".country-select label [value=" + USA + "]").property("checked")) {
-			console.log("Yes it's checked");
-		} else (
-			console.log("Nope it ain't!")
-		);
+		for (var i = 0; i < checkArray.length; i++) {
+			var countryName = checkArray[i].country;
+			// var countryName = "New_Zealand";
 
-		// d3.selectAll(".country-select label [value=USA]")
-		// 	.style("opacity",0);
+			if (d3.select(".country-select label [value=" + countryName + "]").property("checked")) {
+				displayArray.push(checkArray[i]);
+			} 
+
+			// else {
+			// 	// console.log("Nope " + countryName + " ain't checked!");
+			// 	displayArray.splice(i, 1);
+
+			// 	// displayArray[i].choice = 0;
+			// };
+		};
 
 		updateBars();
 	}
@@ -307,12 +331,7 @@ function draw(data) {
 		.attr("transform", "translate(" + 0 + "," + (height + margin.top) + ")")
 		.call(xAxis);			
 
-	/*	Rotate the x axis text by 45 degrees so that it is legible */
-	d3.selectAll(".x text")
-		.attr("text-anchor", "left")
-		.attr("transform", function(d) {
-			return "translate(" + (this.getBBox().width / 2 ) + "," + 2 + "), rotate( 45 " + this.getBBox().x + " " + this.getBBox().y + ")";
-		});
+
 
 
 
@@ -320,6 +339,8 @@ function draw(data) {
 
 	/* Transition the height of the bars to the ac or the cc value */
 	function updateBars() {
+
+
 
 		/* Update the header */
 		d3.select(".outer-wrapper #year").html([displayYear]);
@@ -373,14 +394,84 @@ function draw(data) {
 			yScale.domain([0, d3.max(displayArray, function(d) { return d.choice;} )]);
 		};
 
+
+
 		//Select…
 		var bars = svg.selectAll("rect")
 				.data(displayArray, function(d, i) {
 					return d.country;
-				})
+				});
 
+
+
+
+		//Enter…
+		bars.enter()
+			.append("rect")
+			.transition()
+			.duration(duration)
+			.attr("width", xScale.rangeBand())
+			.attr("x", function(d, i){
+				
+				return xScale(i); 
+			})
+			.attr("y", function(d){
+				return margin.top + yScale(d.choice); 
+			})
+			.attr("height", function(d){
+				return height - yScale(d.choice);
+			});	
+
+		//Update…
+		bars.transition()
+			.duration(duration)
+			.attr("width", xScale.rangeBand())
+			.attr("x", function(d, i){
+				console.log(i + "  " + xScale(i));
+				return xScale(i); 
+			})
+			.attr("y", function(d){
+				return margin.top + yScale(d.choice); 
+			})
+			.attr("height", function(d){
+				return height - yScale(d.choice);
+			});
+	
+			//Exit…
+			bars.exit()
+				.transition()
+				.style("opacity", 0)
+				.remove();
+
+		/* Redefine the X Ordinal Scale */
+		xScale.domain(displayArray.map(function(d) { return d.country; }))
+			.rangeRoundBands([margin.left,(width + margin.right)], 0.1);
+
+
+		// console.log(displayArray.map(function(d) { return d.country; }));
+
+		//Draw X axis
+		svg.select(".outer-wrapper .x")
+			.transition()
+			.duration(duration)
+			.call(xAxis);
+
+		/*	Rotate the x axis text by 45 degrees so that it is legible */
+		d3.selectAll(".x text")
+			.attr("text-anchor", "left")
+			.attr("transform", function(d) {
+				return "translate(" + (this.getBBox().width / 2 ) + "," + 2 + "), rotate( 45 " + this.getBBox().x + " " + this.getBBox().y + ")";
+			});
+
+		/* Call the Y axis to adjust it to the new scale */
+		svg.select(".outer-wrapper .y")
+			.transition()
+			.duration(duration)
+			.call(yAxis);
+			
 		/* Colour the bars and apply the tooltip function */
 		bars.style("fill", allBars)
+			.style("opacity", 0.25)
 			.on("mouseover", function(d) {
 				
 				/*	Create a var to hold the tooltip text string */
@@ -472,46 +563,7 @@ function draw(data) {
 					});
 			});
 
-
-		//Enter…
-		bars.enter()
-			.append("rect")
-			.attr("width", xScale.rangeBand())
-			.attr("x", function(d, i){
-				return xScale(i); 
-			})
-			.attr("y", function(d){
-				return margin.top + yScale(d.choice); 
-			})
-			.attr("height", function(d){
-				return height - yScale(d.choice);
-			});
-
-		//Update…
-		bars.transition()
-			.duration(duration)
-			.attr("width", xScale.rangeBand())
-			.attr("x", function(d, i){
-				return xScale(i); 
-			})
-			.attr("y", function(d){
-				return margin.top + yScale(d.choice); 
-			})
-			.attr("height", function(d){
-				return height - yScale(d.choice);
-			});
-					
-			//Exit…
-			bars.exit()
-				.transition()
-				.style("opacity", 0)
-				.remove();
-
-		/* Call the Y axis to adjust it to the new scale */
-		svg.select(".outer-wrapper .y")
-			.transition()
-			.duration(duration)
-			.call(yAxis);
+			// svg.call(xAxis);
 
 
 	}
